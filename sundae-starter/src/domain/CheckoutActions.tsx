63baf229be92d8ab5a7d4a455c6df9e23920364e
blazelon <jonathanvox@mail.ru>
2024-05-08 20:24:00 +0530

    add typescript

---
+++ sundae-starter/src/domain/CheckoutActions.tsx
@@ -0,0 +1,7 @@
+export const CheckoutActions = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
---
+++ sundae-starter/src/domain/__tests__/CheckoutActions.spec.tsx
@@ -0,0 +1,9 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import {CheckoutActions} from "../CheckoutActions";
+
+describe('checkout action flow', () => {
+    render(<CheckoutActions />)
+
+    
+})
+
