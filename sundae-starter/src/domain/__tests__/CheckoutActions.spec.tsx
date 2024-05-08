52636653e40ccc816343951cbe1c03e46a514e12
blazelon <jonathanvox@mail.ru>
2024-05-08 22:42:11 +0530

    setup types for jest-dom

--- sundae-starter/src/domain/__tests__/CheckoutActions.spec.tsx
+++ sundae-starter/src/domain/__tests__/CheckoutActions.spec.tsx
@@ -4,6 +4,11 @@ import {CheckoutActions} from "../CheckoutActions";
 describe('checkout action flow', () => {
     render(<CheckoutActions />)
 
-    
+    const chkEl = screen.getByRole('checkbox', { name: /terms/i })
+    const btnEl = screen.getByRole('button', { name: /checkout/i })
+
+    fireEvent.click(chkEl)
+
+    // expect(btnEl).toBeE
 })
 
--- sundae-starter/vite.config.ts
+++ sundae-starter/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     // https://github.com/vitest-dev/vitest/blob/main/examples/react-testing-lib/vite.config.ts
     globals: true,
     environment: "jsdom",
-    setupFiles: "./src/setupTests.js",
+    setupFiles: "./src/setupTests.ts",
     // you might want to disable the `css: true` line, since we don't have
     // tests that rely on CSS -- and parsing CSS is slow.
     // I'm leaving it in here becasue often people want to parse CSS in tests.
